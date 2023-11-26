ea00f3733633f0e1d1a6327629c395354aa462bb
Tamiriscoelho <111026976+Tamiriscoelho@users.noreply.github.com>
2023-11-26 14:13:07 -0300

    criando a tela de login

---
+++ resenhasfilmesreact/src/Components/Login/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+import loginImage from '../../assets/Login.png';
+
+//Para que esse código  ess componete seja exibido vou alterar o código do App.js 
+export default function Login(){
+  return(
+      <div className='login-container'>
+        <section className='form'>
+          <img src={loginImage} alt="Login" id='img1'/>
+          <form>
+            <h1>Login</h1>
+            <input type='text' placeholder='Username'/>
+            <input type='password' placeholder='Password'/>
+            <button class='button' type="submit">Login</button>
+          </form>
+        </section>
+      </div>
+  )
+}
