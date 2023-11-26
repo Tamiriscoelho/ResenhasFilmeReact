187e7cd360147d6ea219c54f1e4af4249eb9f7bc
Tamiriscoelho <111026976+Tamiriscoelho@users.noreply.github.com>
2023-11-26 15:54:25 -0300

    configurando as rotas

--- resenhasfilmesreact/src/App.js
+++ resenhasfilmesreact/src/App.js
@@ -1,11 +1,11 @@
 //Os componentes react são funções javascript que retornam html aqui nos temos uma mistura de html e javasccript conhecido com jsx javascript xml vá para a function App()
 import React from 'react';
-import Login from './Components/Login';
+import Routes from './routes';
 import './StyleGlobal.css';
 
 // usando a conexão bidirecional websockte qualquer alteração feita na function App() a pág vai ser atualizada
+//Usando componete Routes usando o roteamento defido
 function App() {
-return (<Login/>)
-
+return (<Routes/>)
 }
 export default App;
---
+++ resenhasfilmesreact/src/routes.js
@@ -0,0 +1,20 @@
+//arquivo para definir as rotas  após  instalar o pacote npm i react-router-dom   
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from "./Components/Login";
+
+//definido o componente de rotas
+export default function MyRoutes() {
+  return(
+    //BrowserRouter usado para definir todas as rotas o 
+    //dentro do  <Routes> a rota é localizada e quando é encontrada o processo é finalizado
+    //Route para definir a rota adicionamos um path ou seja um caminho com a rota raiz.
+    //Compononte route pra fazer a renderização da inteface componente ou uma pagina quando o caminho especifico for encontrado ele vai renderizar passando exact para que seja rederizado a rota especifica pasando o componente.
+    //vá para o arquivo App.js
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Login/>}/>
+        </Routes>
+      </Router>  
+  );
+}
